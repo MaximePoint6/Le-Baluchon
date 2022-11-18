280415514c1cd51e6947f2d876baed16169445c4
Maxime Point <maximepoint6@gmail.com>
2022-11-18 16:48:18 +0100

    setting up the project + colors + tabBar

--- Le BaluchonTests/Le_BaluchonTests.swift
+++ Le BaluchonTests/Le_BaluchonTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import Le_Baluchon
 
-final class Le_BaluchonTests: XCTestCase {
+final class LeBaluchonTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
