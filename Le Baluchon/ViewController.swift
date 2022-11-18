02c0f00ba54ed6960dae8858fe7a9725a748dc97
Maxime Point <maximepoint6@gmail.com>
2022-11-18 14:38:28 +0100

    Initial Commit

---
+++ Le Baluchon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Le Baluchon
+//
+//  Created by Maxime Point on 18/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
